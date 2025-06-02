7b3c3998f1411fbe21400f06557b741f94bfddc2
GilpiDv <gilgonzalez981@gmail.com>
2025-06-02 00:37:47 -0600

    add basic routing

---
+++ src/layouts/Layout.tsx
@@ -0,0 +1,18 @@
+import { Outlet } from "react-router-dom"
+import Header from "../components/Header"
+
+export default function Layout() {
+  return (
+    <>
+        {/* Renders the header for all the pages inside the <Route element={<Layout />} /> component in routes.tsx */}
+        {/* so you don't have to add the header on each page (there can be more components than the Header). */}
+        <Header />
+
+        {/* Outlet component renders the content of the pages inside the <Route element={<Layout />} /> component */}
+        {/* The content of each page will show depending on the route path you are on. */}
+        <main className="container mx-auto py-16">
+            <Outlet />
+        </main>
+    </>
+  )
+}
