6ff2c63d48fb592ff3078d671214a093f37cddf4
GilpiDv <gilgonzalez981@gmail.com>
2025-06-02 02:15:10 -0600

    store filters data in state

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState, type ChangeEvent } from "react";
 import { NavLink, useLocation } from "react-router-dom"
 import { useAppStore } from "../stores/useAppStore";
 
 export default function Header() {
+
+    const [searchFilters, setSearchFilters] = useState({
+        ingredient: '',
+        category: ''
+    });
 
     const { pathname } = useLocation();
 
@@ -12,7 +17,14 @@ export default function Header() {
 
     useEffect(() => {
         fetchCategories();
-    }, [])
+    }, []);
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+        setSearchFilters({
+            ...searchFilters,
+            [e.target.name] : e.target.value
+        })
+    }
 
     return (
         <header className={isHome ? 'bg-[url(/bg.jpg)] bg-center bg-cover' : 'bg-slate-800'}>
@@ -53,6 +65,8 @@ export default function Header() {
                                 id="ingredient" 
                                 className="p-3 w-full rounded-lg focus:outline-none bg-white"
                                 placeholder="Name or Ingredient. e.g. Vodka, Tequila, Coffee..."
+                                value={searchFilters.ingredient}
+                                onChange={handleChange}
                             />
                         </div>
                         <div className="space-y-4">
@@ -66,6 +80,8 @@ export default function Header() {
                                 name="category" 
                                 id="category" 
                                 className="p-3 w-full rounded-lg focus:outline-none bg-white"
+                                value={searchFilters.category}
+                                onChange={handleChange}
                             >
                                 <option value="">Select...</option>
                                 {categories.drinks.map(category => (
