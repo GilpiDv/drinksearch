7fcb4d9af03031b500909fa2c8b5283f3362e136
GilpiDv <gilgonzalez981@gmail.com>
2025-06-03 18:33:16 -0600

    improve page load performance

--- src/router.tsx
+++ src/router.tsx
@@ -1,17 +1,23 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import IndexPage from './views/IndexPage';
-import FavoritesPage from './views/FavoritesPage';
 import Layout from './layouts/Layout';
 
+const FavoritesPage = lazy(() => import('./views/FavoritesPage'))
+
 export default function AppRouter() {
-  return (
-    <BrowserRouter>
-        <Routes>
-            <Route element={<Layout />}>
-                <Route path='/' element={<IndexPage />} />
-                <Route path='/favorites' element={<FavoritesPage />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route element={<Layout />}>
+                    <Route path='/' element={<IndexPage />} />
+                    <Route path='/favorites' element={
+                        <Suspense fallback="loading">
+                            <FavoritesPage />
+                        </Suspense>
+                    } />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    )
 }
